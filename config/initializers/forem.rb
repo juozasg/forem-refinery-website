73b8b0c99dd266e5140cde34ce48b2956781a4c3
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-11-21 11:56:34 -0800

    Sitename

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -4,4 +4,7 @@ Forem.user_class = "Refinery::User"
 Forem.user_profile_links = true
 Forem.email_from_address = "'#{@app_name}' <no-reply@#{@app_host}>"
 Forem.avatar_user_method = "avatar_url" # Method called on user to return an image url for a non-gravatar custom avatar
-Forem.site_name = "My Forums"
+Forem.site_name = "WSA Forums"
+
+
+
